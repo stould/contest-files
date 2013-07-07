0cca94c8660e46fb46991cb525e5304a253a1238
aajjbb <jefersonlsiq@gmail.com>
2013-07-07 13:20:57 -0300

    updating standard file

--- Stardard.cpp
+++ Stardard.cpp
@@ -39,10 +39,11 @@ template<typename T> T lcm(T a, T b) {
 
 template<typename T> void chmin(T& a, T b) { a = (a > b) ? b : a; }
 template<typename T> void chmax(T& a, T b) { a = (a < b) ? b : a; }
+int in() { int x; scanf("%d", &x); return x; }
 
 using namespace std;
 
-typedef long long ll;
+typedef long long Int;
 typedef unsigned uint;
 
 int main(void) {
