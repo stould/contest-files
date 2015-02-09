8c4d3ca9508070f8d21c6a5b5c1db71a9f5b9c98
aajjbb <aajjbb@aajjbb.com>
2015-02-09 01:32:47 -0200

    added Grapevine

--- SPOJ/GeracaoDeEnergia.cpp
+++ SPOJ/GeracaoDeEnergia.cpp
@@ -21,67 +21,84 @@ const Int INF = 100101010010101010LL;
 const int MAXN = 10010;
 
 int N, V;
+int ans;
 int X[MAXN], Y[MAXN], C[MAXN];
-int deg[MAXN], prv[MAXN];
+vector<int> graph[MAXN];
 bool vis[MAXN];
 
-int dfs(int node, int gen) {	
-	int ans = 0;
-	
-	if (gen >= V) {
-		if (!vis[node]) {
-			ans = 1;
+#define IN getc( stdin )
+
+void fastint( register int *n ){
+    register char c;
+    *n = 0;
+    while(!isdigit((c = IN)));
+    ungetc(c, stdin);
+    while(c = IN){
+        switch( c ){
+            case ' ': return;
+            case '\n': return;
+            default: (*n) *= 10; (*n) += ( c - '0' ); 
+        }
+    }
+}
+
+void dfs(int node, int p) {
+	for (int i = 0; i < (int) graph[node].size(); i++) {
+		int nxt = graph[node][i];
+
+		if (nxt != p) {
+			dfs(nxt, node);
 		}
-		vis[node] = true;
-	} else {
-		vis[node] = true;
-		
-		ans = dfs(prv[node], gen + C[node]);
 	}
 
-	return ans;
+	if (C[node] < V) {
+		C[p] += C[node];
+	} else {
+		ans += 1;
+	}
+
+	graph[node].clear();
+	
 }
 
 int main(void) {
-	for ( ; scanf("%d%d", &N, &V) == 2; ) {
+	for ( ; ; ) {
+		fastint(&N);
+		if (N == 0) break;
+		
+		fastint(&V);
+
+		
 		if (N == 0 && V == 0) {
 			break;
 		}
 
 		for (int i = 0; i < N; i++) {
-			scanf("%d%d%d", &X[i], &Y[i], &C[i]);
+			fastint(&X[i]);
+			fastint(&Y[i]);
+			fastint(&C[i]);
 
+			if (i == 0) continue;
+			
 			double dst = INF;
 			int index = -1;
 			
 			for (int j = 0; j < i; j++) {
-				double curr_dst = hypot(X[i] - X[j], Y[i] - Y[j]);
+				double curr_dst = (X[i] - X[j]) * (X[i] - X[j]) + (Y[i] - Y[j]) * (Y[i] - Y[j]);
 
 				if (curr_dst < dst) {
 					dst = curr_dst;
 					index = j;
 				}
 			}
-			
-			deg[index] += 1;
-			prv[i] = index;			
+			graph[index].push_back(i);
 		}
 
-		vector<int> leaf;
-
-		for (int i = 0; i < N; i++) {
-			if (deg[i] == 0) {
-				leaf.push_back(i);
-			}
-		}
-
-		int ans = 0;
+		ans = 0;
+		dfs(0, -1);
 		
-		for (int i = 0; i < (int) leaf.size(); i++) {
-			ans += dfs(leaf[i], C[leaf[i]]);
-		}
-
 		printf("%d\n", ans);
 	}
+	
     return 0;
 }
