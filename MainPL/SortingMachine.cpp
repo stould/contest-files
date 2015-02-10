02315441ce18db471581466924efbbac50135e6a
aajjbb <aajjbb@aajjbb.com>
2015-02-10 13:50:10 -0200

    job changes

--- MainPL/SortingMachine.cpp
+++ MainPL/SortingMachine.cpp
@@ -30,20 +30,32 @@ int main(void) {
 		P[i] = in();
 	}
 
+	int id = 0, inv = 0;
+	
 	for (int i = 0; i < N - K; i++) {
 		int m = INT_MAX;
+		int curr_inv = 0;
+		
 		for (int j = 0; j < K; j++) {
 			chmin(m, P[i + j]);
+
+			if (P[i] > m) {
+				curr_inv += 1;
+			}
 		}
-		if (m != P[i]) {
-			sort(P + i, P + i + K);
-			break;
+		if (curr_inv > inv) {
+			inv = curr_inv;
+			id = i;
 		}
 	}
 
+	sort(P + id, P + id + K);
+	
 	for (int i = 0; i < N; i++) {
 		printf("%d ", P[i]);
 	}
 
-    return 0;
+	printf("\n");
+	
+	return 0;
 }
