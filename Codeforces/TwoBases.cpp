9144427aa341b1dc1d24f29dec54c38d7619c406
aajjbb <aajjbb@aajjbb.com>
2015-11-25 18:21:38 -0200

    job

---
+++ Codeforces/TwoBases.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+template<typename T> T gcd(T a, T b) {
+    if(!b) return a;
+    return gcd(b, a % b);
+}
+template<typename T> T lcm(T a, T b) {
+    return a * b / gcd(a, b);
+}
+
+template<typename T> void chmin(T& a, T b) { a = (a > b) ? b : a; }
+template<typename T> void chmax(T& a, T b) { a = (a < b) ? b : a; }
+int in() { int x; scanf("%d", &x); return x; }
+
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#define debug(args...)
+#else
+#define debug(args...) fprintf(stderr,args)
+#endif
+
+typedef long long Int;
+typedef unsigned long long uInt;
+typedef unsigned uint;
+
+int N, M, BN, BM;
+int P[50], Q[50];
+
+int main(void) {
+	cin >> N >> BN;
+
+	for (int i = 0; i < N; i++) {
+		cin >> P[i];
+	}
+
+	cin >> M >> BM;
+	
+	for (int i = 0; i < M; i++) {
+		cin >> Q[i];
+	}
+
+	
+	return 0;
+}
