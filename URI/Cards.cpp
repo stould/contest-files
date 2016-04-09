9f138cc9a7b7a8b9cbba887652f440ae142ca6b3
aajjbb <aajjbb@aajjbb.com>
2016-04-08 21:05:22 -0300

    adding small from CodeJam 2016

--- URI/Cards.cpp
+++ URI/Cards.cpp
@@ -14,21 +14,21 @@ Int V[MAXN];
 
 int main(void) {
     for ( ; scanf("%d", &N) == 1; ) {
-		for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             scanf("%lld", &V[i]);			
         }
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N - i; j++) {
-				if (i == 0) {
-					dp[j][j] = V[j];
-				} else if (i == 1) {
-					dp[j][j + i] = max(V[j], V[j + 1]);
-				} else {
-					dp[j][j + i] = max(V[j] + min(dp[j + 2][j + i], dp[j + 1][j + i - 1]), V[j + i] + min(dp[j][j + i - 2], dp[j + 1][j + i - 1]));
-				}
-			}
-		}
-		printf("%lld\n", dp[0][N-1]);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                if (i == 0) {
+                    dp[j][j] = V[j];
+                } else if (i == 1) {
+                    dp[j][j + i] = max(V[j], V[j + 1]);
+                } else {
+                    dp[j][j + i] = max(V[j] + min(dp[j + 2][j + i], dp[j + 1][j + i - 1]), V[j + i] + min(dp[j][j + i - 2], dp[j + 1][j + i - 1]));
+                }
+            }
+        }
+        printf("%lld\n", dp[0][N-1]);
     }
     return 0;
 }
