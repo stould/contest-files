a931d86c1534283a00b074af7904022e6310eaac
aajjbb <aajjbb@aajjbb.com>
2016-04-19 18:24:21 -0300

    fixin zonk

--- LiveArchive/ProtectingZonk.cpp
+++ LiveArchive/ProtectingZonk.cpp
@@ -29,69 +29,84 @@ const int INF = INT_MAX / 3;
 int N, C, D;
 int U, V;
 vector<int> tree[MAXN];
-int dp[MAXN][5][5];
+int dp[MAXN][5][5][5][5];
 
-int func(int node, int prev, int down, int up_rem) {
-	if (tree[node].size() == 1 && prev != -1) {
-		if (up_rem == 0) {
-			return 0;
-		} else if (up_rem == 1) {
-			return min(C, D);
-		} else if (up_rem == 2) {
-			return D;
-		} else {
-			return INF;
-		}
-	} else if (up_rem > 2) {
-		return INF;
-	} else {
-		int& ans = dp[node][down][up_rem];
+//
+//pr = previous edge
+//uc = pre-previous edge
+//nx = next edge
+//hs = 1 if node is obligated to use a sargeant bot
+
+int func(int node, int prev, int pr, int uc, int nx, int hs) {
+    if (tree[node].size() == 1 && prev != -1) {
+        if (pr == 1) {
+            return 0;
+        } else if (pr == 0 && uc == 1) {
+            return min(C, D);
+        } else {
+            return D;
+        }
+    } else {
+        int& ans = dp[node][pr][uc][nx][hs];
 		
-		if (ans == -1) {
-			ans = INF;
+        if (ans == -1) {
+            ans = INF;
+            
+            if (hs) {
+                ans = D;
+                
+                for (int i = 0; i < (int) tree[node].size(); i++) {
+                    int u = tree[node][i];
 
-			int not_use = 0;
-			int use_soldier = C;
-			
-			for (int i = 0; i < (int) tree[node].size(); i++) {
-				if (tree[node][i] != prev) {
-					not_use += func(tree[node][i], node, max(0, down - 1), up_rem + 1);
-				}
-			}
-			ans = not_use;
-		}
-		return ans;
-	}
+                    ans += func(u, node, 1, 1, 1, 0);
+                }             
+            } else {            
+                int use_d = D;
+                
+                for (int i = 0; i < (int) tree[node].size(); i++) {
+                    int u = tree[node][i];
+                    
+                    use_d += func(u, node, 1, 1, 1);
+                }
+
+            }
+            chmin(ans, use_d);
+
+            if (uc) {
+                
+            }
+        }
+        
+        return ans;
+    }
 }
 
 int main(void) {
-	for ( ; cin >> N >> C >> D; ) {
-		if (N == 0 && C == 0 && D == 0) break;
+    for ( ; cin >> N >> C >> D; ) {
+        if (N == 0 && C == 0 && D == 0) break;
 
-		for (int i = 0; i < MAXN; i++) {
-			tree[i].clear();
-		}
+        for (int i = 0; i < MAXN; i++) {
+            tree[i].clear();
+        }
 		
-		for (int i = 0; i < N - 1; i++) {
-			cin >> U >> V;
+        for (int i = 0; i < N - 1; i++) {
+            cin >> U >> V;
 			
-			tree[U].push_back(V);
-			tree[V].push_back(U);
-		}
+            tree[U].push_back(V);
+            tree[V].push_back(U);
+        }
+	
+        int ans = INF;
+        
+        for (int i = 1; i <= N; i++) {
+            if (tree[i].size() == 1) {
+                memset(dp, -1, sizeof(dp));
+                chmin(ans, func(i, -1, 1, 1, 0));
+                cout << i << " " << func(i, -1, 1, 1, 0) <<endl;
+            }
+        }
 
-		
-
-		int ans = INF;
-		
-		for (int i = 1; i <= N; i++) {
-			if (tree[i].size() == 1) {
-				memset(dp, -1, sizeof(dp));
-				chmin(ans, func(i, -1, 1, 1, 0));
-				cout << i << " " << func(i, -1, 1, 1, 0) <<endl;
-			}
-		}
-
-		cout << ans << endl;
-	}
-	return 0;
+        cout << ans << endl;
+    }
+    return 0;
 }
