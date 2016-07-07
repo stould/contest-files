b87782b7395661354e430c37b4da0042ab3bfd26
aajjbb <jefersonlsiq@gmail.com>
2016-07-07 13:13:39 -0300

    adding cf 361 files

--- Codeforces/MikeAndChocolateThieves.cpp
+++ Codeforces/MikeAndChocolateThieves.cpp
@@ -10,7 +10,6 @@ template<typename T> T lcm(T a, T b) {
 
 template<typename T> void chmin(T& a, T b) { a = (a > b) ? b : a; }
 template<typename T> void chmax(T& a, T b) { a = (a < b) ? b : a; }
-int in() { int x; scanf("%d", &x); return x; }
 
 using namespace std;
 
@@ -24,20 +23,33 @@ typedef long long Int;
 typedef unsigned long long uInt;
 typedef unsigned uint;
 
-Int N;
+Int M;
 
 int main(void) {
-    cin >> N;
-    bool flag = false;
-    for (Int a = 0; a * 1234 <= N && !flag; a++) {
-        for (Int b = 0; a * 1234 + b * 123456 <= N && !flag; b++) {
-            for (Int c = 0; c * 1234567 + b * 123456 + a * 1234 <= N && !flag; c++) {
-                if (a * 1234 + b * 123456 + c * 1234567 == N) {
-                    flag = true;
-                }
-            }
+    cin >> M;
+
+    Int l = 1LL, h = 100000000000000000L, m;
+    Int ans = -1;
+
+    while (l <= h) { 
+        m = l + (h - l) / 2;
+
+        Int ways = 0;
+
+        for (Int i = 2; ; i++) {
+            if (i * i * i > m) break;
+            ways += m / (i * i * i);
+        }
+
+        if (ways == M) {
+            ans = m;
+            h = m - 1;
+        } else if (ways > M) {
+            h = m - 1;
+        } else {
+            l = m + 1;
         }
     }
-    cout << flag << "\n";
+    cout << ans << "\n";
     return 0;
 }
