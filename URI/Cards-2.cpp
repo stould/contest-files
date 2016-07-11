29a64cf34482fbd3646b9f42c91ab8a574f70a17
aajjbb <jefersonlsiq@gmail.com>
2016-07-11 17:24:00 -0300

    dell changes

--- URI/Cards-2.cpp
+++ URI/Cards-2.cpp
@@ -14,27 +14,51 @@ int in() { int x; scanf("%d", &x); return x; }
 
 using namespace std;
 
-#ifdef ONLINE_JUDGE
-#define debug(args...)
-#else
-#define debug(args...) fprintf(stderr,args)
-#endif
-
 typedef long long Int;
 typedef unsigned long long uInt;
 typedef unsigned uint;
 
 const int MAXN = 10005;
+const Int INF = 100001010010100101LL;
 
 int N;
 int P[MAXN];
-Int ans[MAXN];
+Int dp[2][MAXN];
 
 int main(void) {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
     while (cin >> N) {
         for (int i = 0; i < N; i++) {
             cin >> P[i];
+
+            dp[0][i] = P[i];
+
+            if (i > 0) {
+                dp[1][i - 1] = max(P[i - 1], P[i]);
+            }
         }
+
+        for (int i = 3; i < N; i += 2) {
+            int parity = i % 2;
+
+            for (int j = 0; j + i < N; j++) {
+                //buff[j] = max(buff[j], P[j] + min(dp[parity][j + 2], dp[parity][j + 1]));
+                //buff[j] = max(buff[j], P[j + i] + min(dp[parity][j], dp[parity][j + 1]));
+                dp[parity ^ 1][j] = -INF;
+                dp[parity ^ 1][j] = max(dp[parity ^ 1][j], P[j] + min(dp[parity][j + 2], dp[parity][j + 1]));
+                dp[parity ^ 1][j] = max(dp[parity ^ 1][j], P[j + i] + min(dp[parity][j], dp[parity][j + 1]));
+            }
+
+            for (int j = 0; j < N; j++) {
+                dp[parity][j] = dp[parity ^ 1][j];
+            }
+        }
+
+        Int ans = max(dp[0][0], dp[1][0]);
+
+        cout << ans << "\n";
     }
     return 0;
 }
