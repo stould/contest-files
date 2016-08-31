be9c927a3e84c67b0956c9420c177bc4c76922b6
aajjbb <jefersonlsiq@gmail.com>
2016-08-31 16:12:09 -0300

    fixing spacings in suffix array

--- LiveArchive/WatchingTheKangoroo.cpp
+++ LiveArchive/WatchingTheKangoroo.cpp
@@ -24,80 +24,58 @@ int T, N, M;
 pair<int, int> P[MAXN];
 int K[MAXN];
 
-vector<pair<int, int> > vL, vR;
+struct event {
+    char kind;
+    int in, pos;
 
-bool cmp1(pair<int, int> a, pair<int, int> b) {
-	return a.second < b.second;
-}
-bool cmp2(pair<int, int> a, pair<int, int> b) {
-	return a.second < b.second;
-}
+    event(char kind, int in, int pos): kind(kind), in(in), pos(pos) {}
 
-int checkL(int pos) {
-	int l = 0, h = N, m;
-	int ans = 0;
-			
-	for ( ; l <= h; ) {
-		m = (l + h) / 2;
+    bool operator<(const event& e) const {
+        int si = 0;
+        int sj = 0;
 
-		//		printf("L = %d %d - %d\n", vL[m].first, vL[m].second, ans);
+        if (kind == 'c') {
+            if (in) {
+                si = P[pos].first;
+            } else {
+                si = P[pos].second;
+            }
+        } else {
+            si = K[pos];
+        }
+        
+        if (e.kind == 'c') {
+            sj = K[pos];
+        }
 
-		if (pos <= vL[m].second) {
-			if (pos >= vL[m].first) {
-				//				printf("L = done = %d\n", pos);
-				ans = max(ans, pos - vL[m].first);
-			}
-			h = m - 1;			
-		} else {
-			l = m + 1;
-		}
-	}
-	
-	return ans;
-}
+        
+        return si < sj;
+    }
+};
 
-int checkR(int pos) {
-	int l = 0, h = N, m;
-	int ans = 0;
-			
-	for ( ; l <= h; ) {
-		m = (l + h) / 2;
-		
-		//		printf("R = %d %d - %d\n", vR[m].first, vR[m].second, ans);
-		if (pos >= vR[m].first) {
-			if (pos <= vR[m].second) {
-				ans = max(ans, vR[m].second - pos);
-			}
-			l = m + 1;
-		} else {
-			h = m - 1;			
-		}
-	}
-	
-	return ans;
-}
+vector<event> ve;    
 
 int main(void) {
-	scanf("%d", &T);
-	for (int t = 1; t <= T; t++) {
-		scanf("%d%d", &N, &M);
+    scanf("%d", &T);
+    
+    for (int t = 1; t <= T; t++) {
+        scanf("%d%d", &N, &M);
 				
-		for (int i = 0; i < N; i++) {
-			scanf("%d%d", &P[i].first, &P[i].second);
-			int mid = (P[i].first + P[i].second) / 2;
-			vL.push_back(make_pair(P[i].first, mid));
-			vR.push_back(make_pair(mid, P[i].second));
-		}
+        for (int i = 0; i < N; i++) {
+            scanf("%d%d", &P[i].first, &P[i].second);
+            ve.push_back(event('c', 1, i));
+            ve.push_back(event('c', 0, i));
+        }
 
-		sort(vL.begin(), vL.end(), cmp1);
-		sort(vR.begin(), vR.end(), cmp2);
+        printf("Case %d:\n", t);
 
-		printf("Case %d:\n", t);
+        for (int i = 0; i < M; i++) {
+            scanf("%d", &K[i]);
+            ve.push_back(event('l', 1, i));
+            ve.push_back(event('l', 0, i));
+        }
 
-		for (int i = 0; i < M; i++) {
-			scanf("%d", &K[i]);
-			printf("%d\n", max(checkL(K[i]), checkR(K[i])));
-		}		
-	}
+        sort(ve.begin(), ve.end());
+    }
     return 0;
 }
